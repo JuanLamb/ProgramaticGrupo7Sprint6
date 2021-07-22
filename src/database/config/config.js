992d55494b947a292765b5fe57c930299ae671aa
JuanLamb <juancruzlambrechts@gmail.com>
2021-07-21 22:21:40 -0300

    Lambrechts config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -17,12 +17,11 @@ module.exports = {
         "dialect": "mysql"
       },
     "production": {
-      "username": process.env.DB_USERNAME,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_DATABASE,
-      "host": process.env.DB_HOST,
-      "port": process.env.DB_PORT,
-      "dialect": process.env.DB_DIALECT
+      "username": "root",
+      "password": null,
+      "database": "database_production",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
     }
   }
   
